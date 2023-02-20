f1fe6c81c2b339f7d6e46b52792b2e7e294ed579
joaoalberis <alberisjoao@hotmail.com>
2023-02-20 08:27:53 -0300

    Criação do metodo criarPostagem(forum), metodo para ver as postagens que o dev ja fez postagensFeitas(), criação do metodo desinscreverBootcamp(bootcamp), Criação do metodo para obter um ranking baseado no xp dos devs por bootcamp getRankingOrdenadoPorXp(bootcamp) e edição do metodo celularTotalXp para calcular adicionar o xp das postagens feitas!

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,15 +1,58 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> postagensFeitas = new LinkedHashSet<>();
+
+    private static Map<String, Double> ranking = new HashMap();
+
+
+    public void criarPostagem(Forum forum){
+        this.postagensFeitas.add(forum);
+        forum.getPostagens().add(forum);
+    }
+
+    public String postagensFeitas(){
+        if (this.getPostagensFeitas().isEmpty()) return "O Dev " + this.getNome() + " não possui nenhuma postagem";
+        String postagem = "Abaixo todas as postagens do dev: " + this.getNome() + "\n";
+        for (Conteudo postage : this.getPostagensFeitas()){
+            postagem += "Titulo: " + postage.getTitulo() + "\n";
+            postagem += "Conteúdo: " + postage.getDescricao() + "\n";
+        }
+        return postagem;
+    }
 
     public void inscreverBootcamp(Bootcamp bootcamp){
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void desinscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.removeAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().remove(this);
+    }
+
+    public static Map<String, Double> getRankingOrdenadoPorXp(Bootcamp bootcamp) {
+        Map<String, Double> rank = getRanking(bootcamp);
+        return ranking.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+
+    private static Map<String, Double> getRanking(Bootcamp bootcamp){
+        for (Dev devs : bootcamp.getDevsInscritos()){
+            double xp = devs.calcularTotalXp();
+            ranking.put(devs.getNome(), xp);
+        }
+        return ranking;
     }
 
     public void progredir() {
@@ -24,9 +67,14 @@ public class Dev {
 
     public double calcularTotalXp() {
         Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
+        Iterator<Conteudo> iterator1 = this.postagensFeitas.iterator();
         double soma = 0;
         while(iterator.hasNext()){
             double next = iterator.next().calcularXp();
+            soma += next;
+        }
+        while (iterator1.hasNext()){
+            double next = iterator1.next().calcularXp();
             soma += next;
         }
         return soma;
@@ -37,6 +85,13 @@ public class Dev {
                 .sum();*/
     }
 
+    public Set<Conteudo> getPostagensFeitas() {
+        return postagensFeitas;
+    }
+
+    public void setPostagensFeitas(Set<Conteudo> postagensFeitas) {
+        this.postagensFeitas = postagensFeitas;
+    }
 
     public String getNome() {
         return nome;
@@ -72,6 +127,13 @@ public class Dev {
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+        return Objects.hash(nome);
+    }
+
+    @Override
+    public String toString() {
+        return "" +
+                "postagensFeitas={" + this.postagensFeitas +
+                '}';
     }
 }
